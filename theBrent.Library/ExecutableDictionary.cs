aed2c3c32cb110396797cfa17d910442a593aef7
Brent Maxwell <brent@thebrent.net>
2024-04-25 18:06:09 -0400

    First commit

---
+++ theBrent.Library/ExecutableDictionary.cs
@@ -0,0 +1,47 @@
+﻿namespace theBrent.Library
+{
+    public class ExecutableDictionary<T> : Dictionary<T, Action> where T : notnull
+    {
+        public void Eval(T key)
+        {
+            if (ContainsKey(key))
+            {
+                this[key].Invoke();
+            }
+            else
+            {
+                this[default].Invoke();
+            }
+        }
+    }
+
+    public class ExecutableDictionary<T, T1> : Dictionary<T, Action<T1>> where T : notnull
+    {
+        public void Eval(T key, T1 p1)
+        {
+            if (ContainsKey(key))
+            {
+                this[key].Invoke(p1);
+            }
+            else
+            {
+                this[default].Invoke(p1);
+            }
+        }
+    }
+
+    public class ExecutableDictionary<T, T1, T2> : Dictionary<T, Action<T1, T2>> where T : notnull
+    {
+        public void Eval(T key, T1 p1, T2 p2)
+        {
+            if (ContainsKey(key))
+            {
+                this[key].Invoke(p1, p2);
+            }
+            else
+            {
+                this[default].Invoke(p1, p2);
+            }
+        }
+    }
+}
